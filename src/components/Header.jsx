c87d2ada0a392057a8796020742c68cc3ae4f517
tire-regroover <nick.savolainen@gmail.com>
2025-12-16 16:43:20 -0500

    ok

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 export default function Header() {
     return (
         <header>
+            <a id="home-button" href="/"><img id="home" src="./images/marker.ico" alt="Home" /></a>
             <img id="logo" src="./images/banana.jpg" alt="Logo" />
             <h1>my dum travel journal.</h1>
         </header>
